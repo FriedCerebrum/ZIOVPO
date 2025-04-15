51a0549b5af2c4752922b37c04f7efb55ca21652
FriedCerebrum <iw.kuz8@gmail.com>
2025-04-15 15:41:40 +0300

    1

--- src/main/java/com/example/signatureapp/SignatureAppApplication.java
+++ src/main/java/com/example/signatureapp/SignatureAppApplication.java
@@ -2,12 +2,13 @@ package com.example.signatureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.signatureapp.model")
 public class SignatureAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SignatureAppApplication.class, args);
     }
-
 }
